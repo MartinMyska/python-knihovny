7e785d2350d05b70a6b5edb12606856005fc2bb8
MartinMyska <martin.mysska@gmail.com>
2025-02-20 13:36:51 +0100

    weel_01: rouded set - mnozina

--- 01/mnozina_zaokrouhlenych.py
+++ 01/mnozina_zaokrouhlenych.py
@@ -2,7 +2,7 @@
 # množinu zaokrouhlených hodnot
 
 def mnozina_zaokrouhlenych(vstup):
-    return [round(num_uneven, 1) for num_uneven in vstup]
+    return {round(num_uneven) for num_uneven in vstup}
 
 
 mnozina = [1.4567, 2, 345, 345.00, 0.001]
