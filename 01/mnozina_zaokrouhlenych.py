b3963951ca17bf6e420c1fe519018c879d2804ac
MartinMyska <martin.mysska@gmail.com>
2025-02-20 08:01:30 +0100

    week_01: mnozina zaokrouhlenych

--- 01/mnozina_zaokrouhlenych.py
+++ 01/mnozina_zaokrouhlenych.py
@@ -1,4 +1,10 @@
-# Napiš funkci, která vrátí ze iterovatelného vstupu množinu zaokrouhlených hodnot
+# Napiš funkci, která vrátí ze iterovatelného vstupu
+# množinu zaokrouhlených hodnot
 
 def mnozina_zaokrouhlenych(vstup):
-    ...
+    return [round(num_uneven, 1) for num_uneven in vstup]
+
+
+mnozina = [1.4567, 2, 345, 345.00, 0.001]
+rounded_list = mnozina_zaokrouhlenych(mnozina)
+print(rounded_list)
